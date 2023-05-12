5e2bf1d7e558068e978c7687116569cc0e0acc1a
manami-sato <manamisato2001@gmail.com>
2023-05-13 04:43:56 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  compiler: {
-    styledComponents: true,
-  },
+  // experimental: {
+  //   appDir: true,
+  // },
+  // compiler: {
+  //   styledComponents: true,
+  // },
 };
 
 module.exports = nextConfig;
