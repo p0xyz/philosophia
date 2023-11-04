0686388d927a51950022efd1b03c9a4e95530660
manami-sato <manamisato2001@gmail.com>
2023-11-04 15:45:40 +0900

    fix: 日本語の設定を追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html>
+      <Html lang="ja">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Montserrat&family=League+Spartan:wght@300;600&display=swap"
