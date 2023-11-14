a1c37128ab3c1e34b1993012972995699ee07ee2
manami-sato <manamisato2001@gmail.com>
2023-11-14 22:20:32 +0900

    fix: manifest.json の指定を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class Document extends NextDocument {
@@ -10,12 +9,10 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Poiret+One&family=Montserrat&family=League+Spartan:wght@300;600&display=swap"
             rel="stylesheet"
           />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
-          {/* <ColorModeScript /> */}
           <Main />
           <NextScript />
         </body>
